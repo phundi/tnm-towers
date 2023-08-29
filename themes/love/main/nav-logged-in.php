b31c6e3ae72afc34c86220811f28d8d3990c3b47
phundi <kennethkapundi1@gmail.com>
2023-08-29 11:43:14 +0200

    Showing Live Videos

--- themes/love/main/nav-logged-in.php
+++ themes/love/main/nav-logged-in.php
@@ -132,6 +132,16 @@
 									</li>
 									<li class="divider" tabindex="-1"></li>
 								<?php } ?>
+
+                                
+                                <?php if ($config->agora_live_video == 1) { ?>
+                                    <li>
+                                        <a href="<?php echo $site_url;?>/live-users" data-ajax="/live-users" class="<?php if($data['name'] == 'live-users'){ echo 'active';}?>">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M17 9.2l5.213-3.65a.5.5 0 0 1 .787.41v12.08a.5.5 0 0 1-.787.41L17 14.8V19a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v4.2zm0 3.159l4 2.8V8.84l-4 2.8v.718zM3 6v12h12V6H3zm2 2h2v2H5V8z"></path></svg> <?php echo __( 'Live Videos' );?>
+                                        </a>
+                                    </li>
+                                <?php } ?>
+                                
 								<li>
                                     <a href="<?php echo $site_url;?>/@<?php echo $profile->username;?>" data-ajax="/@<?php echo $profile->username;?>" class="waves-effect"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M4 22a8 8 0 1 1 16 0h-2a6 6 0 1 0-12 0H4zm8-9c-3.315 0-6-2.685-6-6s2.685-6 6-6 6 2.685 6 6-2.685 6-6 6zm0-2c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4z" /></svg> <?php echo __( 'Profile' );?></a>
                                 </li>

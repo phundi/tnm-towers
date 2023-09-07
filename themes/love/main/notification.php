56a631f1277e045cf624d26085269f6c1272b910
phundi <kennethkapundi1@gmail.com>
2023-09-07 09:55:04 +0200

    Removed User Details for Non Pros

--- themes/love/main/notification.php
+++ themes/love/main/notification.php
@@ -1,7 +1,7 @@
 <div class="dt_notifs <?php if($notification->seen == 0 ){ echo 'noti_not_seen'; }else{ echo ''; }?>">
     <a href="javascript:void(0);" data-ajax="<?php echo $notification->url;?>" class="valign-wrapper">
         <div class="avatar">
-            <?php if( in_array( $notification->type ,array('approve_receipt','disapprove_receipt','coinpayments_canceled','coinpayments_approved') ) ) {?>
+            <?php if( ((int)$pro != 1 ) || in_array( $notification->type ,array('approve_receipt','disapprove_receipt','coinpayments_canceled','coinpayments_approved') ) ) {?>
                 <img src="<?php echo $theme_url;?>assets/img/icon.png" />
             <?php }else{ ?>
                 <img src="<?php echo $avater;?>" alt="<?php echo $username;?>" />
@@ -9,7 +9,9 @@
         </div>
         <div class="n_detail">
             <p>
-                <?php if( !in_array( $notification->type ,array('approve_receipt','disapprove_receipt','coinpayments_canceled','coinpayments_approved') ) ) {?><b <?php echo $style;?>><?php echo $username;?></b><br><?php }?>
+                <?php 
+                if ((int)$pro != 1 ){$username = "Some One";}
+                if( !in_array( $notification->type ,array('approve_receipt','disapprove_receipt','coinpayments_canceled','coinpayments_approved') ) ) {?><b <?php echo $style;?>><?php echo $username;?></b><br><?php }?>
 
                 <?php echo $text;?>
             </p>

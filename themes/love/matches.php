b510ede3884ff2305090549ecaf61027cbccea5b
phundi <kennethkapundi1@gmail.com>
2023-09-07 10:50:05 +0200

    Locked Matches page to unsubscribes users

--- themes/love/matches.php
+++ themes/love/matches.php
@@ -11,6 +11,9 @@
 			<div class="container-fluid dt_ltst_users">
 				<div class="dt_home_rand_user">
 					<h4 class="bold"><?php echo __( 'Matches' );?></h4>
+					
+					<?php if( ( (int)auth()->is_pro == 1 || $config->pro_system == 0 ) || isGenderFree((int)auth()->gender) === true){?>
+
 					<div class="row" id="matches_container" data-nomore="<?php echo __('No more users to show.');?>">
                         <?php
                         if(empty($data['matches'])){
@@ -24,6 +27,18 @@
                         <a href="javascript:void(0);" id="btn_load_more_matches" data-lang-nomore="<?php echo __('No more users to show.');?>" data-ajax-post="/loadmore/matches" data-ajax-params="page=2&tags=<?php echo route(2);?>" data-ajax-callback="callback_load_more_matches" class="btn waves-effect load_more"><?php echo __('Load more...');?></a>
                     <?php }?>
                 </div>
+                
+                <?php }else{ ?>
+					<div class="col s12"><div class="dt_sections">
+					<h5 id="_load_more" class="empty_state">
+						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M9,4A4,4 0 0,1 13,8A4,4 0 0,1 9,12A4,4 0 0,1 5,8A4,4 0 0,1 9,4M9,6A2,2 0 0,0 7,8A2,2 0 0,0 9,10A2,2 0 0,0 11,8A2,2 0 0,0 9,6M9,13C11.67,13 17,14.34 17,17V20H1V17C1,14.34 6.33,13 9,13M9,14.9C6.03,14.9 2.9,16.36 2.9,17V18.1H15.1V17C15.1,16.36 11.97,14.9 9,14.9M15,4A4,4 0 0,1 19,8A4,4 0 0,1 15,12C14.53,12 14.08,11.92 13.67,11.77C14.5,10.74 15,9.43 15,8C15,6.57 14.5,5.26 13.67,4.23C14.08,4.08 14.53,4 15,4M23,17V20H19V16.5C19,15.25 18.24,14.1 16.97,13.18C19.68,13.62 23,14.9 23,17Z"></path></svg>
+						<?php echo __('Subscribe to view who matched you!');?>
+						<br><br><br>
+						<a href="<?php echo $site_url;?>/pro" data-ajax="/pro" class="prema"><?php echo __( 'go_premium' );?></a>
+					</h5>
+					</div>
+				<?php }?>
+                    
 			</div>
 			<!-- Interest -->
 		</div>

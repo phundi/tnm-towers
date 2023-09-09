f2420e6098950e35471f9cd72973f28c3984c5ac
phundi <kennethkapundi1@gmail.com>
2023-09-09 15:33:59 +0200

    Hiding location Symbol in search filters

--- themes/love/find-matches.php
+++ themes/love/find-matches.php
@@ -136,7 +136,7 @@ $_gender_text = '';
 						<div class="col s12 m4">
 							<h5><?php echo __('District');?></h5>
 							<div class="valign-wrapper dt_hm_filtr_loc">
-								<label title="<?php echo __('My location');?>">
+								<label style="display:none;" title="<?php echo __('My location');?>">
 									<input type="checkbox" class="filled-in" <?php if(!empty($data['find_match_data']) && !empty($data['find_match_data']['located'])){ ?>checked="checked"<?php }?> id="is_my_location">
 									<b><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12,11.5A2.5,2.5 0 0,1 9.5,9A2.5,2.5 0 0,1 12,6.5A2.5,2.5 0 0,1 14.5,9A2.5,2.5 0 0,1 12,11.5M12,2A7,7 0 0,0 5,9C5,14.25 12,22 12,22C12,22 19,14.25 19,9A7,7 0 0,0 12,2Z" /></svg></b>
 								</label>
@@ -590,7 +590,7 @@ $_gender_text = '';
 	</div>
 </div>
 <script>
-	
+
 $(document).ready(function(){
 	$('#my_country').on('change',() => {
 		$('.located_at').html(`&nbsp;&nbsp;<?php echo __('located_at');?> <span id="located">${$("#my_country option:selected" ).text()}</span>`);

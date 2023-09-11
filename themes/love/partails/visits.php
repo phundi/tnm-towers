f64d6c02f0db462318cad5a4e3e1b3297abefc42
phundi <kennethkapundi1@gmail.com>
2023-09-11 09:54:12 +0200

    Visits from malawi to District

--- themes/love/partails/visits.php
+++ themes/love/partails/visits.php
@@ -9,7 +9,7 @@
         <div class="card-content">
             <a href="<?php echo $site_url;?>/@<?php echo $row->username?>" data-ajax="/@<?php echo $row->username?>"><span class="card-title"><?php echo ($row->first_name !== '' ) ? $row->first_name . ' ' . $row->last_name : $row->username;?> <?php if((int)abs(((strtotime(date('Y-m-d H:i:s')) - $row->lastseen))) < 60 && (int)$row->online == 1) { echo '<div class="useronline"></div>'; }?></span></a>
             <p><?php echo __( 'Visited' );?> <span class="time ajax-time age" title="<?php echo $row->created_at;?>"><?php echo get_time_ago( strtotime($row->created_at) );?></span></p>
-			<p><?php echo __( 'from' );?> <?php echo $row->country_txt?></p>
+			<p><?php echo __( 'from' );?> <?php echo $row->district?></p>
         </div>
     </div>
 </div>

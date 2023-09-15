af0be9600cb57d4128d831b8b1161bbc843bbeef
phundi <kennethkapundi1@gmail.com>
2023-09-15 09:43:24 +0200

    Added new file

--- cronjob.php
+++ cronjob.php
@@ -2,7 +2,6 @@
 require_once realpath(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'bootstrap.php';
 // To delete expired pro membership
 DeleteExpiredProMemebership();
-var_dump("INN");
 // To delete expired boost me users in find-matches page
 DeleteExpiredBoosts();
 // To delete expired xVisits in Popularity page

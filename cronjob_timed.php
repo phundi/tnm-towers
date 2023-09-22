b2faf83bea01bb100b1bbab9f9bdf49003bb8cfe
phundi <kennethkapundi1@gmail.com>
2023-09-22 16:04:54 +0200

    Resending message

--- cronjob_timed.php
+++ cronjob_timed.php
@@ -1,4 +1,4 @@
 <?php
 require_once realpath(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'bootstrap_cronjob.php';
 
-SendQueueSMS('One week free subscription has been given to your account as we celebrate one week old of malovings.com. We love you');
+SendQueueSMS('One week free subscription has been given to your account as we celebrate one week old of www.malovings.com. We love you ! Invite your friends as anyone joining will be receiving 1 week free subscription');
